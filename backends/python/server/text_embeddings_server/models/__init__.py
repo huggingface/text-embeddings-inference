c27350ed7c2decb915006c9e45ba270a378fd61d
Liu, Kaixuan <kaixuan.liu@intel.com>
2025-03-17 16:31:06 -0400

    catch specific exception
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- backends/python/server/text_embeddings_server/models/__init__.py
+++ backends/python/server/text_embeddings_server/models/__init__.py
@@ -67,8 +67,13 @@ def get_model(model_path: Path, dtype: Optional[str], pool: str):
                 )
             try:
                 return FlashBert(model_path, device, datatype)
-            except:
-                return DefaultModel(model_path, device, datatype, pool, trust_remote=TRUST_REMOTE_CODE)
+            except FileNotFoundError as e:
+                logger.info(
+                    "Do not have safetensors file for this model, use default transformers model path instead"
+                )
+                return DefaultModel(
+                    model_path, device, datatype, pool, trust_remote=TRUST_REMOTE_CODE
+                )
         if config.architectures[0].endswith("Classification"):
             return ClassificationModel(
                 model_path, device, datatype, trust_remote=TRUST_REMOTE_CODE
