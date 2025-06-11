9f85bf3b6e8d4ec1aa8c1ae56af3d603c907501b
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-06-11 07:06:14 +0000

    Use `HiddenAct.forward` instead of `match`

--- backends/candle/src/models/jina_code.rs
+++ backends/candle/src/models/jina_code.rs
@@ -284,11 +284,7 @@ impl JinaCodeBertLayer {
         let hidden_states = self.up_gated_layer.forward(&hidden_states)?;
         let non_gated = hidden_states.i((.., .., 0..self.intermediate_size))?;
         let gated = hidden_states.i((.., .., self.intermediate_size..))?;
-        let gated = match self.act {
-            HiddenAct::Gelu => gated.gelu(),
-            HiddenAct::Relu => gated.relu(),
-            HiddenAct::Swiglu => gated.silu(),
-        }?;
+        let gated = self.act.forward(&gated)?;
         let hidden_states = (non_gated * gated)?;
         let hidden_states = self.down_layer.forward(&hidden_states)?;
 
