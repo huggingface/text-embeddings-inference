5aa761fa4d18049ddd350bea627a90db8521625b
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-11-11 23:01:57 +0900

    Fix `TruncationDirection` by renaming to lowercase

--- router/src/http/types.rs
+++ router/src/http/types.rs
@@ -194,6 +194,7 @@ impl<'__s> ToSchema<'__s> for PredictInput {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Deserialize, ToSchema, Eq, Default)]
+#[serde(rename_all = "lowercase")]
 pub(crate) enum TruncationDirection {
     Left,
     #[default]
