11fe2edbf10577b043b65f079d38d34e651810ba
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2026-01-23 18:27:48 +0100

    Add missing `GeluErf` in `match` over `HiddenAct` for cublast

--- backends/candle/src/layers/cublaslt.rs
+++ backends/candle/src/layers/cublaslt.rs
@@ -59,6 +59,7 @@ impl CublasLtWrapper {
         #[cfg(feature = "cuda")]
         {
             let inner_act = match act {
+                Some(HiddenAct::GeluErf) => Some(Activation::GeluErf),
                 Some(HiddenAct::Gelu) => Some(Activation::Gelu),
                 Some(HiddenAct::Relu) => Some(Activation::Relu),
                 _ => None,
@@ -100,6 +101,7 @@ impl CublasLtWrapper {
         #[cfg(feature = "cuda")]
         {
             let inner_act = match act {
+                Some(HiddenAct::GeluErf) => Some(Activation::GeluErf),
                 Some(HiddenAct::Gelu) => Some(Activation::Gelu),
                 Some(HiddenAct::Relu) => Some(Activation::Relu),
                 _ => None,
