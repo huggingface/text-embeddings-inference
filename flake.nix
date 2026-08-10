944d4ef959d876773649c6e584935531686d32d6
Borys Kabakov <b.a.kabakov@gmail.com>
2026-08-10 17:13:53 +0300

    Add CUDA-enabled shell configuration to flake.nix

--- flake.nix
+++ flake.nix
@@ -16,24 +16,44 @@
       devShells = forAllSystems (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs =
-              with pkgs;
-              [
-                rustup
-                protobuf
-                openssl
-                gcc
-                pkg-config
-              ]
-              ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-                apple-sdk_15
-              ];
+          isLinux = nixpkgs.lib.hasSuffix "-linux" system;
+
+          # 1. Base package set (CPU-only)
+          pkgs = import nixpkgs { inherit system; };
+
+          # 2. CUDA package set (Linux only)
+          pkgsCuda = if isLinux then import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              cudaSupport = true;
+            };
+          } else pkgs;
+
+          cudaPackages = pkgsCuda.cudaPackages_12_8;
+
+          # 3. Create a unified root containing both shared and static library binaries
+          cuda-root = if isLinux then pkgsCuda.symlinkJoin {
+            name = "cuda-root";
+            paths = [
+              cudaPackages.cuda_nvcc
+              cudaPackages.cuda_cudart
+              (pkgsCuda.lib.getStatic cudaPackages.cuda_cudart) # <-- Safely resolves static files
+              cudaPackages.cuda_nvrtc
+              (pkgsCuda.lib.getStatic cudaPackages.cuda_nvrtc)  # <-- Safely resolves static files
+              cudaPackages.cccl
+              cudaPackages.libcurand
+              (pkgsCuda.lib.getStatic cudaPackages.libcurand)  # <-- Safely resolves static files
+              cudaPackages.libcublas
+              (pkgsCuda.lib.getStatic cudaPackages.libcublas)  # <-- Safely resolves static files
+              cudaPackages.libcusparse
+              (pkgsCuda.lib.getStatic cudaPackages.libcusparse) # <-- Safely resolves static files
+            ];
+          } else null;
+
+          # 4. Share common shell configurations
+          commonShellAttrs = {
             venvDir = "./.venv";
-            # Relax compiler strictness for oniguruma's old C code
             CFLAGS = "-Wno-error=old-style-definition -Wno-error=implicit-function-declaration";
             RUSTFLAGS = "-C default-linker-libraries=yes";
             postVenvCreation = ''
@@ -43,7 +63,49 @@
               unset SOURCE_DATE_EPOCH
             '';
           };
+        in
+        {
+          # --- SHELL 1: CPU-only ---
+          default = pkgs.mkShell (commonShellAttrs // {
+            buildInputs = with pkgs; [
+              rustup
+              protobuf
+              openssl
+              gcc
+              pkg-config
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              apple-sdk_15
+            ];
+          });
 
+          # --- SHELL 2: CUDA-enabled ---
+          cuda = (pkgsCuda.mkShell.override { stdenv = cudaPackages.backendStdenv; }) (commonShellAttrs // {
+            buildInputs = with pkgsCuda; [
+              rustup
+              protobuf
+              openssl
+              pkg-config
+            ] 
+            ++ (if isLinux then [
+              cudaPackages.cuda_nvcc
+              cudaPackages.cuda_cudart
+              cudaPackages.cuda_nvrtc
+              cudaPackages.cccl
+              cudaPackages.libcurand
+              cudaPackages.libcublas
+              cudaPackages.libcusparse
+              cudaPackages.cudnn
+            ] else [])
+            ++ pkgsCuda.lib.optionals pkgsCuda.stdenv.isDarwin [
+              apple-sdk_15
+            ];
+
+            shellHook = pkgsCuda.lib.optionalString isLinux ''
+              export CUDA_PATH="${cuda-root}"
+              export CUDA_ROOT="${cuda-root}"
+              export CUDA_INCLUDE_DIR="${cuda-root}/include"
+            '';
+          });
         }
       );
     };
